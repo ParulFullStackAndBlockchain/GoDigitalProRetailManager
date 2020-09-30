84643e9a9dc77fe6064f007e20db882d4a131f12
ParulTiwari <parul.tiwari17@gmail.com>
2020-09-30 20:23:34 +0530

    Add the WebAPI Project
    Adds a blank Web API project.

---
+++ GRMDataManager/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+
+namespace GRMDataManager.Controllers
+{
+    [Authorize]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+
+            return new string[] { "value1", "value2", userId };
+        }
+
+        //Note: In case you need more control over your result set use IHttpActionResult.OK() method
+        //public IHttpActionResult Get()
+        //{
+        //    string userId = RequestContext.Principal.Identity.GetUserId();
+
+        //    return Ok(new string[] { "value1", "value2", userId });
+        //}
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
