df0a16b23a5381ce9a5bf7e842839832eefcb802
ParulTiwari <parul.tiwari17@gmail.com>
2020-10-03 20:38:06 +0530

    Register DB Tables
    The tables for the basic register system.

---
+++ GRMMVCUserInterface/Controllers/RetailManagementController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GRMMVCUserInterface.Controllers
+{
+    public class RetailManagementController : Controller
+    {
+        // GET: RetailManagement
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
