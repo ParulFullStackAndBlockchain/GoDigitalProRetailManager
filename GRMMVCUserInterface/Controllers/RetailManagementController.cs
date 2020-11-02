911472718620a2939f7164fad9e6ad643b0ed596
ParulTiwari <parul.tiwari17@gmail.com>
2020-11-02 19:33:38 +0530

    Add Error View and check Permissions.
    Checks to be sure a user has access and informs them if they do not.

--- GRMMVCUserInterface/Controllers/RetailManagementController.cs
+++ GRMMVCUserInterface/Controllers/RetailManagementController.cs
@@ -37,37 +37,23 @@ namespace GRMMVCUserInterface.Controllers
 
         [HttpGet]
         public async Task<ActionResult> Sales(string serializedModel)
-        {           
-            var productList = await GetAllProducts();
-            List<SelectListItem> listSelectListItems = new List<SelectListItem>();
-            foreach (ProductModel product in productList)
+        {
+            try
             {
-                SelectListItem selectList = new SelectListItem()
+                var productList = await GetAllProducts();
+                List<SelectListItem> listSelectListItems = new List<SelectListItem>();
+                foreach (ProductModel product in productList)
                 {
-                    Text = product.ProductName,
-                    Value = product.Id.ToString()
-                };
-                listSelectListItems.Add(selectList);
-            }
+                    SelectListItem selectList = new SelectListItem()
+                    {
+                        Text = product.ProductName,
+                        Value = product.Id.ToString()
+                    };
+                    listSelectListItems.Add(selectList);
+                }
 
-            ProductsViewModel productsViewModel; 
-            if (null == serializedModel)
-            {
-                productsViewModel = new ProductsViewModel()
-                {
-                    AvailableProducts = listSelectListItems,
-                    ProductsAddedToCart = new List<SelectListItem>(),
-                    ProductsAddedToCartString = "",
-                    Quantity = 1,
-                    SubTotal = 0,
-                    Tax = 0,
-                    Total = 0
-                };
-            }
-            else
-            {
-                productsViewModel = JsonConvert.DeserializeObject<ProductsViewModel>(serializedModel);
-                if (null == productsViewModel)
+                ProductsViewModel productsViewModel;
+                if (null == serializedModel)
                 {
                     productsViewModel = new ProductsViewModel()
                     {
@@ -80,9 +66,36 @@ namespace GRMMVCUserInterface.Controllers
                         Total = 0
                     };
                 }
-            }
+                else
+                {
+                    productsViewModel = JsonConvert.DeserializeObject<ProductsViewModel>(serializedModel);
+                    if (null == productsViewModel)
+                    {
+                        productsViewModel = new ProductsViewModel()
+                        {
+                            AvailableProducts = listSelectListItems,
+                            ProductsAddedToCart = new List<SelectListItem>(),
+                            ProductsAddedToCartString = "",
+                            Quantity = 1,
+                            SubTotal = 0,
+                            Tax = 0,
+                            Total = 0
+                        };
+                    }
+                }
 
-            return View(productsViewModel);
+                return View(productsViewModel);
+            }
+            catch (Exception ex)
+            {
+                //TODO : Log the exception. 
+                if (ex.Message.Equals("Unauthorized"))
+                {
+                    ViewBag.ErrorTitle = $"{ex.Message}";
+                    ViewBag.ErrorMessage = "You do not have permission to interact with the sales form";                   
+                }
+                return View("Error");
+            }
         }
 
         [HttpPost]
